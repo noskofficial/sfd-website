aee0d9f90202f08300ab15961eb1dc604f061202
Rishav Chapagain <gainrishavchap@gmail.com>
2024-09-07 22:15:33 +0545

    Config Files Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}",      // Include all HTML and JS files in the current folder and subfolders
-    "!./node_modules",       // Exclude the node_modules folder
-    "!./**/node_modules",    // Exclude any node_modules folder in subdirectories (if applicable)
+    "./**/*.{html,js}", // Include all HTML and JS files in the current folder and subfolders
+    "!./node_modules", // Exclude the node_modules folder
+    "!./**/node_modules", // Exclude any node_modules folder in subdirectories (if applicable)
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bahnschrift: ["Bahnschrift", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
