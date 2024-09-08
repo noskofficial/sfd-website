f4a0945e1e226daa82b8f1626113bd0b625f9d5a
Anish Neupane <anishnep51@gmail.com>
2024-09-08 15:37:22 +0545

    Updated Navbar with hover effects and link changes

--- js/navbar.js
+++ js/navbar.js
@@ -15,14 +15,14 @@ const navCode = `<nav class="flex flex-row mb-4 w-dvw justify-between px-[1%] md
             </button>
             <!-- Dropdown menu -->
             <div
-                class="hidden absolute group-hover/about:block bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44">
+                class="hidden absolute *:text-black group-hover/about:block bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44 font-normal">
                 <ul class="py-1" aria-labelledby="dropdownLargeButton">
                     <li>
-                        <a href="/html/about.html" class="text-sm hover:bg-gray-100 block px-4 py-2">About
+                        <a href="/html/aboutsfd.html" class="text-sm hover:bg-gray-100 block px-4 py-2">About
                             SFD</a>
                     </li>
                     <li>
-                        <a href="#" class="text-sm hover:bg-gray-100 block px-4 py-2">About NOSK</a>
+                        <a href="https://nosk.org.np/site/html/about.html" class="text-sm hover:bg-gray-100 block px-4 py-2">About NOSK</a>
                     </li>
                     <li>
                         <a href="#" class="text-sm hover:bg-gray-100 block px-4 py-2">Gallery</a>
@@ -33,8 +33,8 @@ const navCode = `<nav class="flex flex-row mb-4 w-dvw justify-between px-[1%] md
                 </ul>
             </div>
         </span>
-        <span id="itinerary"><a href="/html/itinerary.html">Itinerary</a></span>
-        <span id="ourteam"><a href="/html/ourteam.html">Our Team</a></span>
+        <span id="itinerary" class="hover:border-b-2"><a href="/html/itinerary.html">Itinerary</a></span>
+        <span id="team" class="hover:border-b-2"><a href="/html/ourteam.html">Our Team</a></span>
         <span class="group/activities" id="activities">
             <button
                 class="md:hover:bg-transparent md:border-0 pl-3 pr-4 py-2 md:p-0 flex items-center justify-between w-full md:w-auto">Activities
@@ -47,10 +47,10 @@ const navCode = `<nav class="flex flex-row mb-4 w-dvw justify-between px-[1%] md
             </button>
             <!-- Dropdown menu -->
             <div
-                class="hidden absolute group-hover/activities:block bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44">
+                class="hidden absolute group-hover/activities:block bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44 font-normal">
                 <ul class="py-1" aria-labelledby="dropdownLargeButton">
                     <li>
-                        <a href="/html/about.html" class="text-sm hover:bg-gray-100 block px-4 py-2">Project Registration</a>
+                        <a href="https://forms.gle/ZmT2eDPqyLoLj5Sd6" class="text-sm hover:bg-gray-100 block px-4 py-2">Project Registration</a>
                     </li>
                     <li>
                         <a href="#" class="text-sm hover:bg-gray-100 block px-4 py-2">NOSKODE 5.0</a>
@@ -58,8 +58,8 @@ const navCode = `<nav class="flex flex-row mb-4 w-dvw justify-between px-[1%] md
                 </ul>
             </div>
         </span>
-        <span id="faqs"><a href="/html/faqs.html">FAQs</a></span>
-        <span id="contactus"><a href="/html/contactus.html">Contact Us</a></span>
+        <span id="faqs" class="hover:border-b-2"><a href="/html/faqs.html">FAQs</a></span>
+        <span id="contact" class="hover:border-b-2"><a href="/html/contactus.html">Contact Us</a></span>
     </div>
 </nav>
 `
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentPage = splittedPath[splittedPath.length - 1].replace(".html", "")
     const navChildren = document.querySelector("#navlist").children
     for (navChild of navChildren) {
-        if (navChild.id == currentPage) {
+        if (currentPage.includes(navChild.id)) {
             navChild.style = "color: #E4554E"
 
         }
