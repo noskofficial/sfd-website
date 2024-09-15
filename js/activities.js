e55ea71a7e73ab807acedb491612565d22d194e8
Anish Neupane <anishnep51@gmail.com>
2024-09-15 21:24:49 +0545

    Fixed Responsiveness in some devices

--- js/activities.js
+++ js/activities.js
@@ -24,20 +24,20 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 const unclickGen = (activity) => {
 
-    return `<div class='bg-white rounded-2xl w-full xl:w-[345px] 2xl:w-[400px] p-5 flex flex-col gap-4  items-center h-full ${activity.title == "Cake Cutting" ? " sm:hidden lg:flex " : ""}'>
+    return `<div class='bg-white rounded-2xl w-full p-5 pt-8 border-[0.2px] border-[#787676] transition-all drop-shadow-lg hover:drop-shadow-2xl cursor-default flex flex-col gap-4  items-center h-full ${activity.title == "Cake Cutting" ? " sm:hidden lg:flex " : ""}'>
         <img src="${activity.image}" alt="${activity.title}" class="h-20 w-20">
-        <div class="font-bold text-2xl text-center">${activity.title}</div>
-        <div class="font-light text-base">${activity.description}</div>
+        <div class="font-bold text-xl md:text-2xl text-center">${activity.title}</div>
+        <div class="font-light text-sm md:text-base text-center">${activity.description}</div>
     </div>`
 }
 const clickGen = (activity, titleColor, buttonColor) => {
-    return `<div class='bg-white rounded-2xl w-full p-5 flex flex-col gap-4  items-center h-full ${activity.title == "Cake Cutting" ? " sm:hidden lg:flex " : ""}'>
+    return `<div class='bg-white rounded-2xl w-full p-5 flex flex-col gap-4 drop-shadow-lg transition-all hover:drop-shadow-2xl cursor-default border-[0.2px] border-[#787676] items-center h-full ${activity.title == "Cake Cutting" ? " sm:hidden lg:flex " : ""}'>
         <div class="flex w-full items-center">
             <img src="${activity.image}" alt="${activity.title}" class="h-20 w-20">
-            <div class="font-bold text-2xl flex-1 text-center" style="color: ${titleColor}">${activity.title}</div>
+            <div class="font-bold text-xl md:text-2xl flex-1 text-center" style="color: ${titleColor}">${activity.title}</div>
         </div>
-        <div class="font-light text-base flex-1">${activity.description}</div>
-        <a target="_blank" class="text-base w-4/5 h-10 rounded-3xl font-bold text-white items-center flex justify-center hover:bg-transparent" href="${activity.button.link}" style="background-color: ${buttonColor}; border: 1px solid ${buttonColor}" onmouseover="hoverEffect(this)" onmouseout="resetEffect(this)">${activity.button.title}</a>
+        <div class="font-light text-sm md:text-base flex-1 text-center">${activity.description}</div>
+        <a target="_blank" class="text-base w-4/5 h-10 rounded-3xl font-bold text-white items-center flex justify-center transition-all hover:bg-transparent" href="${activity.button.link}" style="background-color: ${buttonColor}; border: 1px solid ${buttonColor}" onmouseover="hoverEffect(this)" onmouseout="resetEffect(this)">${activity.button.title}</a>
     </div>`
 }
 
