f6207c802f3723f11e35049541e806b88f7050c9
Rishav Chapagain <gainrishavchap@gmail.com>
2024-09-16 11:14:39 +0545

    Quality of Life Changes

--- js/faqs.js
+++ js/faqs.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const question = item.querySelector(".faq-question");
     const answer = item.querySelector(".faq-answer");
 
-    question.addEventListener("click", () => {
+    item.addEventListener("click", () => {
       if (answer.style.maxHeight && answer.style.maxHeight !== "0px") {
         answer.style.maxHeight = "0px";
         question.querySelector("span.text-gray-900").textContent = "+";
