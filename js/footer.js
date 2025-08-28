2fce1f0af907763b42697a913a77d31f397ce728
khadkaabishek <abishekkhadka36@gmail.com>
2025-08-28 20:24:04 +0545

    logo for diff devices fixed ...

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,8 @@
-const footerCode = `<section class="mt-7 flex h-80 w-full items-center justify-center bg-celeb-banner bg-no-repeat bg-cover bg-center">
+const footerCode = `<section class="mt-7 flex h-80 w-full items-center justify-center bg-celeb-banner bg-no-repeat bg-cover bg-center overflow-hidden">
   <p class="text-center text-3xl md:text-5xl font-bold text-white px-4">
     CELEBRATE THE FREEDOM OF COLLABORATION
   </p>
 </section>
-
 
 
 
@@ -43,7 +42,7 @@ const footerCode = `<section class="mt-7 flex h-80 w-full items-center justify-c
 
 <section class="h-auto w-full bg-black flex items-center justify-center py-4">
   <p class="text-white text-center text-xs md:text-base">
-    Copyright© Software Freedom Day 2025 x Nepal Open Source Klub
+    Copyright &copy; Software Freedom Day 2025 x Nepal Open Source Klub
   </p>
 </section> `;
 
